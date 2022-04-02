281b6c3fe3e36bf77c8aa5b990c607cf01c0939c
Thais <thhaishs.silva98@gmail.com>
2022-04-02 14:35:09 -0300

    :feat Adicionar Startup e Dados

---
+++ app-web-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ app-web-backend/Models/Dados.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace app_web_backend.Models
+{
+    [Table("Dados")]
+    public class Dados
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o Usuário!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o Nome!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o Email!")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a Senha!")]
+        public int CEP { get; set; }
+     }
+}
